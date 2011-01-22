fe0b0fb82ec11e50edb8c4439f46bfe0958a0303
Brad Heller <brad.heller@gmail.com>
2011-01-22 12:34:20 -0800

    more validation fixes

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -4,6 +4,7 @@ class Brew < ActiveRecord::Base
   has_many :beers
   has_many :tastings
 	
+  validates_presence_of :name, :message => 'A name is required.'
   validates_numericality_of :abv, :message => 'ABV must be a decimal less than 100.', :less_than_or_equal_to => 100, :allow_nil => true
   validates_numericality_of :ibus, :message => 'IBUs must be an integer less than 100.', :less_than_or_equal_to => 100, :allow_nil => true, :only_integer => true
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,6 @@
 class Brewery < ActiveRecord::Base
 	has_many :brews
+  
+  validates_presence_of :name, :message => 'A name is required.'
+  validates_uniqueness_of :username, :case_sensitive => false, :message => 'A brewery by that name already exists.'
 end
