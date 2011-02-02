e59d9a0cf77cbd027e6a7b944fab7a0ff6341ca5
Brad Heller <brad.heller@gmail.com>
2011-02-02 11:26:43 -0800

    Changed order column

--- db/migrate/20110202181212_add_order_to_bottle_sizes.rb
+++ db/migrate/20110202181212_add_order_to_bottle_sizes.rb
@@ -1,10 +1,10 @@
 class AddOrderToBottleSizes < ActiveRecord::Migration
   def self.up
-    add_column :bottle_sizes, :order, :integer
+    add_column :bottle_sizes, :sort_order, :integer
     
     i = 1
     BottleSize.all.each do |b|
-      b.order = i
+      b.sort_order = i
       b.save
       
       i += 1
@@ -12,6 +12,6 @@ class AddOrderToBottleSizes < ActiveRecord::Migration
   end
 
   def self.down
-    remove_column :bottle_sizes, :order
+    remove_column :bottle_sizes, :sort_order
   end
 end
