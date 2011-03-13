2d2a878fadc7f3966667befe9b2c629facea9134
Kris <krishemenway@gmail.com>
2011-03-12 18:00:02 -0800

    fixed tests -- fixed validation on year in beer

--- test/unit/beer_test.rb
+++ test/unit/beer_test.rb
@@ -7,12 +7,14 @@ class BeerTest < ActiveSupport::TestCase
   end
   
   test "should not save a beer in the future" do
-    beer = Beer.new :year => Time.new.year + Beer::ACCEPTED_YEARS_FROM_TODAY + 1
+    beer = beers(:Valid_Beer)
+    beer.year = Time.new.year + Beer::ACCEPTED_YEARS_FROM_TODAY + 1
     assert (not beer.valid?)
   end
   
   test "should not save a beer too far in the past" do
-    beer = Beer.new :year => Beer::YEAR_OF_OLDEST_BEER - 1
+    beer = beers(:Valid_Beer)
+    beer.year = Beer::YEAR_OF_OLDEST_BEER - 1
     assert (not beer.valid?)
   end
 end
