b3f245eada9188377972494dab5ce19be99d9930
Brad Heller <brad.heller@gmail.com>
2011-03-19 12:31:33 -0700

    Removed ability to edit email address from preferences page. We need to think through this more.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,7 @@ class UsersController < ApplicationController
     if params[:user][:state].nil?
       @user.state = nil
     end
-    
-    email = params[:user][:email]
-    existing = User.find_by_email email
-    
-    # It could be the case that the person tried to log in with their FB Connect account
-    # before but failed.
-    if existing and existing.facebook_id and not existing.username
-      # Delete the facebook connect record for this
-      existing.destroy 
-    end
-    
+
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to(cellar_path(@user.username), :notice => 'Your preferences have been saved!') }
