339040bb343b2d06f220aba45f9fea4b58a0e8d5
Brad Heller <brad.heller@gmail.com>
2011-03-25 00:27:28 -0700

    Fixed...so many fucking problems

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -37,14 +37,28 @@ class BeersController < ApplicationController
 
   # GET /beers/1/edit
   def edit
-    @beer = Beer.find(params[:id])
+    @cellar = Cellar.find_by_user User.find_by_username(params[:cellar_id])
     
-    # Format the dates because fuck it sucks to do in views
-    @formatted_finish_aging_at = @beer.finish_aging_at ? @beer.finish_aging_at.strftime("%Y-%m-%d") : ''
-    @formatted_cellared_at = @beer.cellared_at ? @beer.cellared_at.strftime("%Y-%m-%d") : ''
-    
-    @years = (@beer.finish_aging_at.year - @beer.cellared_at.year) unless @beer.cellared_at.nil? or @beer.finish_aging_at.nil?
-    @months = ((@beer.finish_aging_at.month - @beer.cellared_at.month) % 12) unless @beer.cellared_at.nil? or @beer.finish_aging_at.nil?
+    if request.post?
+      unless params.has_key? :beer_id
+        raise ArgumentError "Beer IDs are missing. You need to specify the IDs of beers you want to operate on."
+      end
+
+      @beers = params[:beer_id].map { |i| Beer.find i }
+
+      respond_to do |format|
+        format.html { render :partial => 'beers/multiedit' }
+      end
+    else
+      @beer = Beer.find(params[:id])
+
+      # Format the dates because fuck it sucks to do in views
+      @formatted_finish_aging_at = @beer.finish_aging_at ? @beer.finish_aging_at.strftime("%Y-%m-%d") : ''
+      @formatted_cellared_at = @beer.cellared_at ? @beer.cellared_at.strftime("%Y-%m-%d") : ''
+
+      @years = (@beer.finish_aging_at.year - @beer.cellared_at.year) unless @beer.cellared_at.nil? or @beer.finish_aging_at.nil?
+      @months = ((@beer.finish_aging_at.month - @beer.cellared_at.month) % 12) unless @beer.cellared_at.nil? or @beer.finish_aging_at.nil?
+    end
   end
 
   # POST /beers
@@ -56,8 +70,20 @@ class BeersController < ApplicationController
     if params[:beer][:price] =~ /^\$/
       params[:beer][:price] = params[:beer][:price][1,(params[:beer][:price].length - 1)]
     end
-    
+
+    # If a brew and brewery ID exist in the params then we should load it up
+    if params[:beer].has_key? :brew_id and not params[:beer][:brew_id].blank?
+      brew = Brew.find(params[:beer][:brew_id])
+      params[:beer].delete(:brew_id)
+    elsif params[:beer].has_key? :brew_id and params[:beer][:brew_id].blank?
+      params[:beer].delete(:brew_id)
+    end
+
+    # We just need to ignore any brewery ids sent across for now. We get brewery from brew.
+    params[:beer].delete(:brewery_id) if params[:beer].has_key? :brewery_id
+
     @beer = @cellar.beers.new(params[:beer])
+    @beer.brew = brew unless brew.nil?
     
     if @beer.valid?
       @beer.name = @beer.brew.name
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,6 @@
 class Beer < ActiveRecord::Base
   ACCEPTED_YEARS_FROM_TODAY = 2
+  MAXIMUM_BEER_YEAR = Time.new.year + ACCEPTED_YEARS_FROM_TODAY
   YEAR_OF_OLDEST_BEER = 1800
 
   acts_as_commentable
@@ -11,16 +12,14 @@ class Beer < ActiveRecord::Base
   belongs_to :bottle_size
   
   validates_presence_of :brew_id, :message => "Please select a beer."
-  validates_presence_of :year, :message => "Year is required."
+  validates_presence_of :year, :message => "A year is required. It's (kind of) ok to guess."
   validates_presence_of :cellared_at, :message => "Cellared date is required."
 
-  validates_numericality_of :year, :message => "That doesn't look like a year to me.", :only_integer => true
-  validates_numericality_of :year, :message => "I don't believe you have a beer that old.", :greater_than => YEAR_OF_OLDEST_BEER
-  validates_numericality_of :year, :message => "I don't believe you have that beet yet.", :less_than => Time.new.year + ACCEPTED_YEARS_FROM_TODAY  
+  validates_numericality_of :year, :message => "Year is invalid. Is must be a four digit number between #{YEAR_OF_OLDEST_BEER} and #{MAXIMUM_BEER_YEAR}.", :only_integer => true, :greater_than => YEAR_OF_OLDEST_BEER, :less_than => MAXIMUM_BEER_YEAR
   
   validates_numericality_of :quantity, :message => "Quantity must be a number less than 120.", :less_than => 120
   validates_numericality_of :abv, :message => "ABV must be a decimal less than 150.", :less_than_or_equal_to => 150, :allow_nil => true
-  validates_numericality_of :price, :message => "Invalid price, bro.", :allow_nil => true
+  validates_numericality_of :price, :message => "Please supply a valid price.", :allow_nil => true
   
   def formatted_cellared_at
     self.cellared_at.strftime "%A %B %d, %Y" unless self.cellared_at.nil?
