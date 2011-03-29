4bf7b560e40f89060a5b9853a872cca7ab5ae758
Brad Heller <brad.heller@gmail.com>
2011-03-28 19:01:35 -0700

    FUCK, stupid rails. I should be allowed to have ? in my attributes.

--- db/migrate/20110328050606_add_confirmed_to_users.rb
+++ db/migrate/20110328050606_add_confirmed_to_users.rb
@@ -1,9 +1,9 @@
 class AddConfirmedToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :confirmed?, :boolean
+    add_column :users, :confirmed, :boolean
   end
 
   def self.down
-    remove_column :users, :confirmed?
+    remove_column :users, :confirmed
   end
 end
