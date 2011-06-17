954fd4d7c0dff056a1461eb7f1650490c4368cb5
Brad Heller <brad@aboutus.org>
2011-06-17 10:18:17 -0700

    Attempt to bootstrap user.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,12 @@ role :db,  "ec2-50-19-13-135.compute-1.amazonaws.com"
 
 set :deploy_to, '/www/hoptopus'
 
+set :use_sudo, false
+set :user, 'deploy'
+set :password, '!!abc123'
+set :application, "hoptopus.com"
+set :domain, "ec2-50-19-13-135.compute-1.amazonaws.com"
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
@@ -18,5 +24,15 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :bootstrap_deploy_user do
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"sudo groupadd admin\""
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"sudo useradd -d /home/#{user} -s /bin/bash -m #{user}\""
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"echo #{user}:#{password} | chpasswd\""
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"sudo usermod -a -G admin deploy\""
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"sudo mkdir /home/#{user}/.ssh\""
+    system "ssh -i #{aws_private_key_path} ubuntu@#{domain} \"sudo chown -R #{user}:#{user} /home/#{user}/.ssh\""
+    system "scp -i #{aws_private_key_path} config/deploy_sudoers ubuntu@#{domain}:/etc/sudoers"
   end
 end
