f380ed9771007a79f5d937de169456f609b578bb
Brad Heller <brad@aboutus.org>
2011-06-27 14:30:59 -0700

    Add recent events back in...

--- app/controllers/cellars_controller.rb
+++ app/controllers/cellars_controller.rb
@@ -94,10 +94,10 @@ class CellarsController < ApplicationController
   # GET /cellars
   # GET /cellars.xml
   def index
-    if @user.nil? or @user.should_show_own_events
-      @recent_events = []
+    if current_user.nil? or current_user.should_show_own_events
+      @recent_events = Event.order('created_at DESC').limit(15).all
     else 
-      @recent_events = []
+      @recent_events = Event.order('created_at DESC').limit(15).where('user_id != ?', current_user.id).all
     end
 
     @newest_cellars = Cellar.newest.first(25).distribute(5)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,13 @@
 require 'array'
 
 class HomeController < ApplicationController
-  
   def index
     unless @user.nil?
-#      if @user.nil? or @user.should_show_own_events
-#        @recent_events = Event.order('created_at DESC').limit(15).all
-#      else 
-#        @recent_events = Event.order('created_at DESC').limit(15).where("user_id != ?", @user.id).all
-#      end
-
-      @recent_events = []
+      if current_user.nil? or current_user.should_show_own_events
+        @recent_events = Event.order('created_at DESC').limit(15).all
+      else 
+        @recent_events = Event.order('created_at DESC').limit(15).where('user_id != ?', current_user.id).all
+      end
 
       @newest_cellars = Cellar.newest.first(25).distribute(5)
       @largest_cellars = Cellar.largest.first(25).distribute(5)
@@ -18,5 +15,4 @@ class HomeController < ApplicationController
       render :template => 'cellars/index'
     end
   end
-  
 end
